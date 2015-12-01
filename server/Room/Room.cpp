8c10a04b253d5fb9b049b93ca353e07875831a83
Nicolas charvoz <nicolas.charvoz@epitech.eu>
2015-12-01 15:04:03 +0100

    it compiles ADD FILES TO CMAKE

--- server/Room/Room.cpp
+++ server/Room/Room.cpp
@@ -5,18 +5,20 @@
 // Login   <antoinegarcia@epitech.net>
 //
 // Started on  Tue Dec  1 05:29:21 2015 Antoine Garcia
-// Last update Tue Dec  1 05:32:12 2015 Antoine Garcia
+// Last update Tue Dec  1 15:00:39 2015 Nicolas Charvoz
 //
 
 #include "Room.hh"
 
+Room::Room() {}
+
 Room::Room(const std::string &id):_id(id)
 {}
 
-Room::~Room();
+Room::~Room()
 {}
 
-std::string&	Room::getId() const
+const std::string &Room::getId() const
 {
   return (_id);
 }
