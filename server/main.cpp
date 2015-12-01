be8f996ea8405c9df98f5bd09f5e88ef07af65c5
Louis Audibert <louis.audibert@epitech.eu>
2015-12-01 05:14:27 -0800

    testing EntityFactory and EntityManager

--- server/main.cpp
+++ server/main.cpp
@@ -4,16 +4,31 @@
 // Made by Louis Audibert
 // Login   <audibel@epitech.net>
 //
-// Last update Tue Dec  1 12:10:54 2015 Nicolas Charvoz
+// Last update Tue Dec  1 04:47:53 2015 Louis Audibert
 // Last update Mon Nov 30 05:50:36 2015 Antoine Garcia
 //
 
 #include	"Server.hh"
+#include "EntityManager.hh"
 
 int		main(int ac, char **av)
 {
   //  Server	*s = new Server();
   std::cout << "You've launched the Server of the RType" << std::endl;
+
+  EntityManager *test = new EntityManager();
+
+  test->createEntity();
+  test->createEntity();
+  test->createEntity();
+  AEntity *testpos = test->getEntityById(1);
+  testpos->addSystem(E_POSITION);
+  AEntity *testhealth = test->getEntityById(2);
+  testhealth->addSystem(E_HEALTH);
+
+  test->removeEntityById(3);
+  test->removeEntityById(2);
+  test->removeEntityById(1);
 
   std::cout << "Made by La Pintade" << std::endl;
   return (0);
