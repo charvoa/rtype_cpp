7d6ed7d63dde84f6d1f061ed631416803001ac22
Louis Audibert <louis.audibert@epitech.eu>
2015-12-01 13:08:14 -0800

    Player Factory + id no longer in EntityFactory but in EntityManager

---
+++ server/PlayerFactory.cpp
@@ -0,0 +1,9 @@
+//
+// PlayerFactory.cpp for rtype in /home/audibel/rendu/rtype_cpp/server
+//
+// Made by Louis Audibert
+// Login   <audibel@epitech.net>
+//
+// Started on  Tue Dec  1 07:49:08 2015 Louis Audibert
+// Last update Tue Dec  1 07:49:09 2015 Louis Audibert
+//
