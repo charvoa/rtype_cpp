965c0ecd42b9dead3d346e7a5f1cce7104063d65
girards <nicolas.girardot@epitech.eu>
2015-12-01 12:02:11 +0100

    Remove error compilation

--- client/common/IRenderWindow.hh
+++ client/common/IRenderWindow.hh
@@ -5,11 +5,7 @@
 // Login   <sergeheitzler@epitech.net>
 //
 // Started on  Mon Nov 23 08:20:13 2015 Serge Heitzler
-<<<<<<< HEAD
-// Last update Wed Nov 25 06:33:30 2015 Serge Heitzler
-=======
-// Last update Wed Nov 25 16:36:31 2015 Nicolas Girardot
->>>>>>> 02a251ffe5f072a58c677484f844235795494e85
+// Last update Tue Dec  1 11:43:28 2015 Nicolas Girardot
 //
 
 #ifndef IRENDERWINDOW_HH_
@@ -25,23 +21,6 @@
 
 class  		      IRenderWindow
 {
-<<<<<<< HEAD
-
-  IRenderWindow();
-  ~IRenderWindow();
-
-  virtual void		create(IVideoMode mode, const std::string&, Uint32, const contextSettings&) = 0;
-  virtual void		close() = 0;
-  virtual bool		isOpen() = 0;
-  virtual bool		pollEvent(sf::Event& event) = 0;
-  virtual bool	        waitEvent(sf::Event& event) = 0;
-  virtual IVector2    	getSize() = 0;
-  virtual void		SetVerticalSyncEnabled(bool value) = 0;
-  virtual void		setMouseCursorVisible(bool value) = 0;
-  virtual void		setKeyRepeatRepeatEnabled(bool value) = 0;
-  virtual void		display() = 0;
-  
-=======
 public:
   virtual void		close() = 0;
   virtual bool		isOpen() = 0;
@@ -54,7 +33,6 @@ public:
   virtual void		setKeyRepeatEnabled(bool value) = 0;
   virtual void		display() = 0;
   virtual void		draw(const sf::Drawable &drawable, const sf::RenderStates &states) = 0;
->>>>>>> 02a251ffe5f072a58c677484f844235795494e85
 };
 
 #endif /* !IRENDERWINDOW_HH_ */
--- client/common/IVector2.hh
+++ client/common/IVector2.hh
@@ -5,19 +5,14 @@
 // Login   <sergeheitzler@epitech.net>
 //
 // Started on  Mon Nov 23 08:20:13 2015 Serge Heitzler
-// Last update Mon Nov 30 06:32:36 2015 Serge Heitzler
+// Last update Tue Dec  1 11:50:02 2015 Nicolas Girardot
 //
 
 #ifndef IVECTOR2_HH_
 #define IVECTOR2_HH_
 
-template <typename T>
 class	        IVector2
 {
-
-  IVector2(T x, T y);
-  ~IVector2();
-
 };
 
 #endif /* !IVECTOR2_HH_ */
