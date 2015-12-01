ad61aa2caa718de589085599cc7ade17de72c450
Louis Audibert <louis.audibert@epitech.eu>
2015-12-01 06:02:20 -0800

    adding PlayerManager

---
+++ server/PlayerManager.hh
@@ -0,0 +1,32 @@
+//
+// PlayerManager.hh for rtype in /home/audibel/rendu/rtype_cpp/server
+//
+// Made by Louis Audibert
+// Login   <audibel@epitech.net>
+//
+// Started on  Tue Dec  1 05:44:31 2015 Louis Audibert
+// Last update Tue Dec  1 05:53:54 2015 Louis Audibert
+//
+
+#ifndef _PLAYERMANAGER_HH_
+# define _PLAYERMANAGER_HH_
+
+# include <vector>
+# include "Client.hh"
+# include "Player.hh"
+
+class PlayerManager
+{
+private:
+  std::vector<Player>	_players;
+
+public:
+  PlayerManager();
+  ~PlayerManager();
+
+  bool			createPlayer(Client &client);
+  std::vector<Player>	&getAllPlayers();
+  Player		&getPlayerById(int  id);
+};
+
+#endif /* _PLAYERMANAGER_HH_ */
