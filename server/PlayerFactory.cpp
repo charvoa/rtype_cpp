422cc8cd9667c03d92483f8f21c63b598aba9493
Louis Audibert <louis.audibert@epitech.eu>
2015-12-02 02:38:55 -0800

    Player Factory

--- server/PlayerFactory.cpp
+++ server/PlayerFactory.cpp
@@ -5,5 +5,24 @@
 // Login   <audibel@epitech.net>
 //
 // Started on  Tue Dec  1 07:49:08 2015 Louis Audibert
-// Last update Tue Dec  1 07:49:09 2015 Louis Audibert
+// Last update Wed Dec  2 02:33:26 2015 Louis Audibert
 //
+
+#include "PlayerFactory.hh"
+
+PlayerFactory::PlayerFactory()
+{
+
+}
+
+PlayerFactory::~PlayerFactory()
+{
+  std::cout << "Player Factory destroyed" << std::endl;
+}
+
+Player	*PlayerFactory::createPlayer(int &id, const Client &client)
+{
+  std::cout << "New Player created !" << std::endl;
+  id += 1;
+  return (new Player(id, client));
+}
