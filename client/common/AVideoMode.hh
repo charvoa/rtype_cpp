5c467fad4921a8213d7644df45a195fa9515cd76
girards <nicolas.girardot@epitech.eu>
2015-12-02 13:35:59 +0100

    Change CMakeLists, Remove possible warning with thread

---
+++ client/common/AVideoMode.hh
@@ -0,0 +1,21 @@
+//
+// AVideoMode.hh for VideoMode.cpp in /home/nicolas/rendu/rtype_cpp/client/common
+//
+// Made by Nicolas Girardot
+// Login   <girard_s@epitech.net>
+//
+// Started on  Wed Dec  2 11:37:59 2015 Nicolas Girardot
+// Last update Wed Dec  2 11:39:03 2015 Nicolas Girardot
+//
+
+#include "VideoMode.hh"
+
+VideoMode::VideoMode(unsigned int width, unsigned int height, unsigned int bitsPerPixel)
+{
+
+}
+
+VideoMode::~VideoMode()
+{
+
+}
--- client/common/VideoMode.hh
+++ client/common/VideoMode.hh
@@ -5,19 +5,21 @@
 // Login   <girard_s@epitech.net>
 //
 // Started on  Wed Nov 25 17:23:47 2015 Nicolas Girardot
-// Last update Thu Nov 26 17:26:45 2015 Nicolas Girardot
+// Last update Wed Dec  2 11:39:53 2015 Nicolas Girardot
 //
 
 #ifndef VIDEOMODE_HH_
 #define VIDEOMODE_HH_
 
-#include "AVideoMode.hh"
-
-class VideoMode : public AVideoMode
+class VideoMode
 {
 public:
   VideoMode(unsigned int width, unsigned int height, unsigned int bitsPerPixel = 32);
   ~VideoMode();
+
+  unsigned int width;
+  unsigned int height;
+  unsigned int bitsPerPixel;
 }
 
 #endif
