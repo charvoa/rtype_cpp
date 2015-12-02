74d9ac850b98c2529a4a159b29d557c4a0185d53
Nicolas charvoz <nicolas.charvoz@epitech.eu>
2015-12-02 13:15:23 +0100

    merge

--- server/Game.cpp
+++ server/Game.cpp
@@ -5,7 +5,7 @@
 // Login   <nicolaschr@epitech.net>
 //
 // Started on  Tue Dec  1 17:45:38 2015 Nicolas Charvoz
-// Last update Wed Dec  2 11:35:41 2015 Nicolas Charvoz
+// Last update Wed Dec  2 13:15:16 2015 Nicolas Charvoz
 //
 
 #include "Game.hh"
@@ -31,6 +31,8 @@ void Game::setParameters(Parameters &p)
   _params = p;
 }
 
-bool Game::run() {}
+bool Game::run() {
+  return true;
+}
 
 void Game::addCommandToQueue(ANetwork::t_frame frame) {}
