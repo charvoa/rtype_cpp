4dfb7646a6ee1f38ae37a059bbb0a64091f1c8bd
Nicolas charvoz <nicolas.charvoz@epitech.eu>
2015-12-03 13:18:33 +0100

    Mutex almost done

---
+++ server/Mutex.hpp
@@ -0,0 +1,41 @@
+//
+// MutexUnix.hpp for  in /home/nicolaschr/rendu/rtype_cpp/server
+//
+// Made by Nicolas Charvoz
+// Login   <nicolaschr@epitech.net>
+//
+// Started on  Wed Dec  2 18:06:01 2015 Nicolas Charvoz
+// Last update Wed Dec  2 21:17:30 2015 Nicolas Charvoz
+//
+
+#ifndef MUTEX_HPP_
+# define MUTEX_HPP_
+
+# include "AMutex.hh"
+# include <mutex>
+
+class Mutex : public AMutex {
+
+  std::mutex _mutex;
+
+public:
+
+  Mutex() {
+
+  };
+
+  void lock() {
+    _mutex.lock();
+    _isLocked = true;
+  };
+  void unlock() {
+    _mutex.unlock();
+    _isLocked = false;
+  };
+  std::mutex *getMutex() {
+    return &_mutex;
+  };
+
+};
+
+#endif
