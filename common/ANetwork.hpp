e6ed989460f6177673a9f188079acc5575d41918
Antoine Garcia <antoine.garcia@epitech.eu>
2015-12-05 05:00:05 -0800

    [*]Common : Move ANetwork and ISocket to common and modify include in ANetwork

---
+++ common/ANetwork.hpp
@@ -0,0 +1,51 @@
+//
+// ANetwork.hh for  in /home/nicolaschr/rendu/rtype_cpp/server
+//
+// Made by Nicolas Charvoz
+// Login   <nicolaschr@epitech.net>
+//
+// Started on  Wed Nov 25 16:38:49 2015 Nicolas Charvoz
+// Last update Sat Dec  5 04:54:35 2015 Antoine Garcia
+//
+
+#ifndef ANETWORK_HPP_
+# define ANETWORK_HPP_
+
+# include	<string>
+# include	<ISocket.hpp>
+
+class ANetwork
+{
+public:
+   typedef enum		e_TYPE {
+    TCP_MODE,
+    UDP_MODE
+  }			TYPE;
+
+protected:
+  char			_buffer[4096];
+  ISocket		*_socket;
+
+public :
+  typedef struct	s_frame {
+    short int		_keyPintade;
+    unsigned char	_requestType;
+    unsigned int	_idRequest;
+    int			_crc;
+    int			_sizeData;
+    char		_data[49];
+  }			t_frame;
+
+  ANetwork() {};
+  virtual		~ANetwork() {};
+  virtual void		init(int port_, ANetwork::TYPE type) = 0;
+  virtual void		bind() = 0;
+  virtual void		listen(int) = 0;
+  virtual void		*read(int) = 0;
+  virtual int		write(void *, int) = 0;
+  virtual void		close() = 0;
+  virtual ISocket	*accept() = 0;
+
+};
+
+#endif
