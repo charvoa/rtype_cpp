111b3b905ad10181d533a39a55e497721fec1003
Serge Heitzler <serge.heitzler@epitech.eu>
2015-12-05 06:08:07 -0800

    [*] Client : InputManager assigned to APanel type, rendering and updating with the stack Panel

--- client/common/Client.cpp
+++ client/common/Client.cpp
@@ -5,7 +5,7 @@
 // Login   <girard_s@epitech.net>
 //
 // Started on  Sat Dec  5 10:16:26 2015 Nicolas Girardot
-// Last update Sat Dec  5 04:51:58 2015 Serge Heitzler
+// Last update Sat Dec  5 06:04:52 2015 Serge Heitzler
 //
 
 #include "Client.hh"
@@ -25,17 +25,16 @@ void	Client::Start()
   RenderWindow *window = RenderWindow::getInstance();
   window->setWindow(sf::VideoMode(1920, 1080, 32), "R-Pint");
   window->clear();
-  StartPanel start;
-  InputManager	*startInput = new InputManager(InputType::MENU_INPUT);
+  window->getPanels().push(static_cast<StartPanel*>(PanelFactory::createPanel(PanelFactory::PanelType::START_PANEL)));
   while(window->isOpen())
     {
-      start.update();
-      start.render();
+      window->getPanels().top()->update();
+      window->getPanels().top()->render();      
       window->display();
       sf::Event event;
       while (window->pollEvent(event))
 	{
-	  startInput->methodChecker(event);
+	  window->getPanels().top()->getInputManager().methodChecker(event);
 	}
     }
 }
---
+++ client/common/InputTypeEnum.hh
@@ -0,0 +1,20 @@
+//
+// InputTypeEnum.hh for rtype in /home/sergeheitzler/rendu/rtype_cpp/client/common
+// 
+// Made by Serge Heitzler
+// Login   <sergeheitzler@epitech.net>
+// 
+// Started on  Sat Dec  5 05:56:06 2015 Serge Heitzler
+// Last update Sat Dec  5 05:56:55 2015 Serge Heitzler
+//
+
+#ifndef							INPUTTYPEENUM_HH_
+#define							INPUTTYPEENUM_HH_
+
+typedef enum					InputType
+  {
+    MENU_INPUT,
+    GAME_INPUT
+  }						InputType;
+
+#endif							/* INPUTTYPEENUM_HH_ */
