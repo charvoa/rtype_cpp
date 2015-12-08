967c64532f8d9040cf9d9804f9f521c46005a82b
Antoine Garcia <antoine.garcia@epitech.eu>
2015-12-08 17:10:55 +0100

    [*] Server : Update createRoom sendData

--- server/Room/RoomManager.cpp
+++ server/Room/RoomManager.cpp
@@ -43,7 +43,7 @@ void	RoomManager::createNewRoom(Client &client)
 {
   Room	room(generateId(), client);
   _rooms.push_back(room);
-  std::string	sendData = "player1;" + room.getId() + ";player1;" + "player1";
+  std::string	sendData = "player1;" + room.getId() + "1"";
   ANetwork::t_frame frame = CreateRequest::create((unsigned char)101, CRC::calcCRC(sendData), 0, sendData);
   client.getSocket()->write(reinterpret_cast<void *>(&frame), sizeof(ANetwork::t_frame));
   std::cout << "Create Room With Id" << room.getId() << std::endl;
