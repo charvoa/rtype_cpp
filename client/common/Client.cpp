00aea0b62ccbe6e729758fa24bae7584cacbd9e8
Antoine Garcia <antoine.garcia@epitech.eu>
2015-12-08 12:29:56 -0800

    [*] Server : Delete useless dynamic cast to Socket * in server.cpp

--- client/common/Client.cpp
+++ client/common/Client.cpp
@@ -5,7 +5,7 @@
 // Login   <girard_s@epitech.net>
 //
 // Started on  Sat Dec  5 10:16:26 2015 Nicolas Girardot
-// Last update Tue Dec  8 08:25:40 2015 Serge Heitzler
+// Last update Tue Dec  8 12:24:47 2015 Antoine Garcia
 //
 
 #include <Client.hh>
@@ -54,7 +54,7 @@ void	Client::Start()
   RenderWindow *window = RenderWindow::getInstance();
   _network = new Network();
   _network->init(4253, ANetwork::TCP_MODE);
-  _network->connect("10.16.253.125");
+  _network->connect("0");
   window->setWindow(sf::VideoMode(1920, 1080, 32), "R-Pint");
   window->clear();
 
