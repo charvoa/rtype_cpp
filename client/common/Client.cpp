f17c09fdd97a9459d32ba5da98c2a9bbb6e97e28
Serge Heitzler <serge.heitzler@epitech.eu>
2015-12-08 14:31:41 -0800

    [*] Client : Update debugging informations, and display color in Room

--- client/common/Client.cpp
+++ client/common/Client.cpp
@@ -5,7 +5,7 @@
 // Login   <girard_s@epitech.net>
 //
 // Started on  Sat Dec  5 10:16:26 2015 Nicolas Girardot
-// Last update Tue Dec  8 12:24:47 2015 Antoine Garcia
+// Last update Tue Dec  8 13:51:56 2015 Serge Heitzler
 //
 
 #include <Client.hh>
@@ -54,7 +54,7 @@ void	Client::Start()
   RenderWindow *window = RenderWindow::getInstance();
   _network = new Network();
   _network->init(4253, ANetwork::TCP_MODE);
-  _network->connect("0");
+  _network->connect("192.168.1.25");
   window->setWindow(sf::VideoMode(1920, 1080, 32), "R-Pint");
   window->clear();
 
