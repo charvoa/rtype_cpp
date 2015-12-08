665d344c317a80d420cb719e2b9f38c647aaa137
Nicolas charvoz <nicolas.charvoz@epitech.eu>
2015-12-08 16:42:49 +0100

    [*] Server : main modified with only utility

--- server/CommandManager.cpp
+++ server/CommandManager.cpp
@@ -5,7 +5,7 @@
 // Login   <nicolaschr@epitech.net>
 //
 // Started on  Tue Dec  1 11:32:51 2015 Nicolas Charvoz
-// Last update Sat Dec  5 17:06:21 2015 Joris Bertomeu
+// Last update Tue Dec  8 16:30:44 2015 Nicolas Charvoz
 //
 
 #include <CommandManager.hpp>
@@ -17,5 +17,6 @@ CommandManager::~CommandManager() {}
 bool CommandManager::addFunction(E_COMMAND type, bool (Server::*cmd)(ANetwork::t_frame frame, void *data))
 {
   _commands[type] = cmd;
+
   return true;
 }
--- server/main.cpp
+++ server/main.cpp
@@ -4,7 +4,7 @@
 // Made by Louis Audibert
 // Login   <audibel@epitech.net>
 //
-// Last update Tue Dec  8 15:37:13 2015 Nicolas Charvoz
+// Last update Tue Dec  8 16:35:49 2015 Nicolas Charvoz
 // Last update Mon Nov 30 05:50:36 2015 Antoine Garcia
 //
 
@@ -20,51 +20,15 @@
 #include <ANetwork.hpp>
 #include <CreateRequest.hpp>
 
-AMutex *m;
-
-void *save_page(void *s)
-{
-  m->lock();
-  std::cout << "this thread is called" << std::endl;
-  m->unlock();
-  return s;
-}
-
 int		main(int ac, char **av)
 {
   Server	*s = new Server();
 
   (void)ac;
-  (void)av;
-
-
-  /* TEST */
-  m = new Mutex();
-
-  std::unique_ptr<AThread> t1(new Thread(1));
-  std::unique_ptr<AThread> t2(new Thread(2));
-
-  char str1[] = "http://foo";
-  char str2[] = "http://bar";
-
-  t1->attach(&save_page, (void*)str1);
-  t2->attach(&save_page, (void*)str2);
-
-  t1->run();
-  t2->run();
-  t1->join();
-  t2->join();
-
-  ANetwork::t_frame frame;
-
-  frame = CreateRequest::create(1, 12, 3, "data");
-
-  std::cout << "frame data : " << CreateRequest::getData(frame) << std::endl;
-  /* FIN DU GAME */
 
   try {
-    if (av[1])
-      s->init(atoi(av[1]));
+    // if (av[1])
+    //   s->init(atoi(av[1]));
     s->init(4253);
     std::cout << "You've launched the Server of the RType" << std::endl;
 
