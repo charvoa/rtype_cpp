5352c23aa58fe34ebacbdc6b35d49395159ed3a6
Antoine Garcia <antoine.garcia@epitech.eu>
2015-12-08 12:50:51 -0800

    [*] Server : SendError Room is ok

--- server/Room/Room.cpp
+++ server/Room/Room.cpp
@@ -5,7 +5,7 @@
 // Login   <antoinegarcia@epitech.net>
 //
 // Started on  Tue Dec  1 05:29:21 2015 Antoine Garcia
-// Last update Tue Dec  8 02:57:57 2015 Antoine Garcia
+// Last update Tue Dec  8 12:50:06 2015 Antoine Garcia
 //
 
 #include <Room.hh>
@@ -52,9 +52,10 @@ void	Room::sendRoomPlayerJoin(Client &client)
 
 void	Room::sendError(Client &client)
 {
-	std::string sendData = "Too many players";
+  std::string sendData = "Too many players";
 
-    ANetwork::t_frame frame = CreateRequest::create((unsigned char)104, CRC::calcCRC(sendData), 0, sendData);
+  ANetwork::t_frame frame = CreateRequest::create((unsigned char)104, CRC::calcCRC(sendData), 0, sendData);
+  client.getSocket()->write(reinterpret_cast<void *>(&frame), sizeof(ANetwork::t_frame));
 }
 
 void	Room::addPlayer(Client &client)
@@ -65,7 +66,7 @@ void	Room::addPlayer(Client &client)
       sendPlayerJoin(client);
       sendRoomPlayerJoin(client);
     }
-  //sendError();
+  sendError(client);
 }
 
 std::vector<Client>&	Room::getAllPlayers()
