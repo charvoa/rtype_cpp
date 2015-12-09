3b46bf56048d0db1ed1ca512b71c68d7a321beca
Louis Audibert <louis.audibert@epitech.eu>
2015-12-09 06:59:41 -0800

    merge

--- server/AEntity.hh
+++ server/AEntity.hh
@@ -5,7 +5,7 @@
 // Login   <audibel@epitech.net>
 //
 // Started on  Mon Nov 30 06:37:36 2015 Louis Audibert
-// Last update Wed Dec  9 06:51:33 2015 Louis Audibert
+// Last update Wed Dec  9 06:57:53 2015 Louis Audibert
 //
 
 #ifndef _AENTITY_HH_
@@ -31,9 +31,9 @@ public:
   bool	update(int, int);
   bool	update(int);
   // virtual void update();
-  void	addSystem(E_COMPONENT type);
-  bool	setType(E_ENTITYTYPE type);
-  E_ENTITYTYPE getType() const;
+  void	addSystem(E_Component type);
+  bool	setType(E_EntityType type);
+  E_EntityType getType() const;
   int	getId() const;
   SystemManager *getSystemManager();
 };
