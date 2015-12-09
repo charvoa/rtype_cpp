52e23de3b813473911fe02d33feb41e6cbec9cf8
Nicolas charvoz <nicolas.charvoz@epitech.eu>
2015-12-09 13:39:28 +0100

    [+] Server : created BonusIG

---
+++ server/BonusIG.hpp
@@ -0,0 +1,29 @@
+//
+// BonusIG.hpp for  in /home/nicolaschr/rendu/rtype_cpp/server
+//
+// Made by Nicolas Charvoz
+// Login   <nicolaschr@epitech.net>
+//
+// Started on  Wed Dec  9 13:32:20 2015 Nicolas Charvoz
+// Last update Wed Dec  9 13:36:57 2015 Nicolas Charvoz
+//
+
+#ifndef BONUSIG_HPP_
+# define BONUSIG_HPP_
+
+# include <AEntity.hh>
+# include <string>
+# include <iostream>
+
+class BonusIG : public AEntity {
+
+public:
+
+  BonusIG();
+  ~BonusIG();
+
+private:
+
+};
+
+#endif
