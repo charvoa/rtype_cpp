629925e9196732e5d64e5c97dfebb4cf6e8f20c2
girards <nicolas.girardot@epitech.eu>
2015-12-11 22:44:22 +0100

    [+] Client : Display should be functionnal needs testing with server

--- client/common/GamePanel.hh
+++ client/common/GamePanel.hh
@@ -5,7 +5,7 @@
 // Login   <girard_s@epitech.net>
 //
 // Started on  Fri Dec 11 16:48:25 2015 Nicolas Girardot
-// Last update Fri Dec 11 17:34:23 2015 Nicolas Girardot
+// Last update Fri Dec 11 22:36:43 2015 Nicolas Girardot
 //
 
 
@@ -15,6 +15,7 @@
 #include <APanel.hh>
 #include <PlayerIG.hh>
 #include <RenderWindow.hh>
+#include <Sprite.hh>
 
 class GamePanel : public APanel
 {
@@ -23,13 +24,16 @@ public:
   ~GamePanel();
 
   void render();
+  std::vector<Sprite *> &getSprites();
   PlayerIG	*getPlayerByName(const std::string &name);
   Text	*getScore();
   static void		setLife(const std::string &name, int life);
   static	void	setScore(int);
+  static	void	display(std::vector<std::string> &vector);
 private:
   Text	*_score;
   std::vector<PlayerIG *> _players;
+  std::vector<Sprite *> _sprites;
 };
 
 #endif // GAMEPANEL_H
