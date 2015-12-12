f5dd5322ab319342c4094a1d880ced281608dabf
Antoine Garcia <antoine.garcia@epitech.eu>
2015-12-12 14:08:22 -0800

    push Timer

---
+++ server/Timer.hpp
@@ -0,0 +1,44 @@
+//
+// Timer.hh for Timer in /home/antoinegarcia/rendu/rtype_cpp/server
+//
+// Made by Antoine Garcia
+// Login   <antoinegarcia@epitech.net>
+//
+// Started on  Sat Dec 12 12:52:59 2015 Antoine Garcia
+// Last update Sat Dec 12 13:26:31 2015 Antoine Garcia
+//
+
+#ifndef _TIMER_HPP_
+# define _TIMER_HPP_
+
+#include <chrono>
+#include <ostream>
+
+class Timer {
+  typedef std::chrono::high_resolution_clock high_resolution_clock;
+  typedef std::chrono::seconds	    seconds;
+public:
+  explicit Timer(bool run = false)
+  {
+    if (run)
+      reset();
+  }
+  void reset()
+  {
+    _start = high_resolution_clock::now();
+  }
+  seconds elapsed() const
+  {
+    return std::chrono::duration_cast<seconds>(high_resolution_clock::now() - _start);
+  }
+  template <typename T, typename Traits>
+  friend std::basic_ostream<T, Traits>& operator<<(std::basic_ostream<T, Traits>& out, const Timer& timer)
+  {
+    return out << timer.elapsed().count();
+  }
+private:
+  high_resolution_clock::time_point _start;
+};
+
+
+#endif
