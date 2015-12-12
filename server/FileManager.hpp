e5ba4182b3fbaef4463f40eb8ece2d6f618c5cd1
Joris Bertomeu <joris.bertomeu@epitech.eu>
2015-12-12 00:48:34 -0500

    [*] Server : File + FileManager

---
+++ server/FileManager.hpp
@@ -0,0 +1,28 @@
+//
+// FileManager.hpp for  in /home/jobertomeu/Work/rtype_cpp/server
+//
+// Made by Joris Bertomeu
+// Login   <jobertomeu@epitech.net>
+//
+// Started on  Fri Dec 11 18:19:25 2015 Joris Bertomeu
+// Last update Fri Dec 11 18:56:19 2015 Joris Bertomeu
+//
+
+#ifndef			_FILEMANAGER_HPP_
+# define		_FILEMANAGER_HPP_
+
+# include		<File.hpp>
+
+class		FileManager
+{
+public:
+  explicit	FileManager() {
+
+  };
+
+  virtual	~FileManager(void) {
+
+  };
+};
+
+#endif
