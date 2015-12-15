767966628618246bbd8c19faef4629ed312a54e1
Viveka <Viveka>
2015-12-15 18:01:54 +0100

    [+] Client : new buttons for keyButton

--- client/common/KeyButton.cpp
+++ client/common/KeyButton.cpp
@@ -8,7 +8,7 @@
 KeyButton::KeyButton()
 {
 	_waiting = false;
-	_sprite->setTexture(*(RenderWindow::getInstance())->_ressources->_buttonNormal);
+	_sprite->setTexture(*(RenderWindow::getInstance())->_ressources->_keyButtonNormal);
 }
 
 
@@ -38,7 +38,7 @@ void		KeyButton::reset(std::string const& title)
 
 	(window->getPanels().top()->getLabels().at(_id)).setString(title);
 	setTitle(title);
-	getSprite().setTexture(*(RenderWindow::getInstance())->_ressources->_buttonNormal);
+	getSprite().setTexture(*(RenderWindow::getInstance())->_ressources->_keyButtonNormal);
 	delete loader;
 }
 
@@ -108,7 +108,7 @@ void		KeyButton::setBind(sf::Event event)
 		break;
 	}
 	_waiting = false;
-	getSprite().setTexture(*(RenderWindow::getInstance())->_ressources->_buttonNormal);
+	getSprite().setTexture(*(RenderWindow::getInstance())->_ressources->_keyButtonNormal);
 	delete loader;
 }
 
@@ -118,12 +118,12 @@ bool		KeyButton::updateOnPress(std::pair<unsigned int, unsigned int> pair)
 
 	if (pair.first >= rect.first.first && pair.first <= (rect.first.first + rect.second.first) && pair.second >= rect.first.second && pair.second <= (rect.first.second + rect.second.second))
 	{
-		getSprite().setTexture(*(RenderWindow::getInstance())->_ressources->_buttonHighlight);
+		getSprite().setTexture(*(RenderWindow::getInstance())->_ressources->_keyButtonHighlight);
 		_waiting = true;
 	}
 	else
 	{
-		getSprite().setTexture(*(RenderWindow::getInstance())->_ressources->_buttonNormal);
+		getSprite().setTexture(*(RenderWindow::getInstance())->_ressources->_keyButtonNormal);
 		_waiting = false;
 	}
 	return false;
