c80afa240d0785d7aa292bd6d7053853cd25a037
Louis Audibert <louis.audibert@epitech.eu>
2015-12-15 02:23:36 -0800

    [*] Server: adding types in E_EntityType

--- server/E_EntityType.hh
+++ server/E_EntityType.hh
@@ -5,7 +5,7 @@
 // Login   <audibel@epitech.net>
 //
 // Started on  Mon Nov 30 06:42:37 2015 Louis Audibert
-// Last update Mon Dec 14 17:07:45 2015 Nicolas Charvoz
+// Last update Tue Dec 15 02:21:39 2015 Louis Audibert
 //
 
 #ifndef _E_ENTITYTYPE_HH_
@@ -13,12 +13,15 @@
 
 enum E_EntityType
   {
-    E_BOT,
-    E_PLAYER,
-    E_RIFLE,
-    E_MISSILE,
-    E_LASER,
-    E_INVALID
+    E_PLAYER1 = 1,
+    E_PLAYER2 = 2,
+    E_PLAYER3 = 3,
+    E_PLAYER4 = 4,
+    E_BOT = 4,
+    E_RIFLE = 5,
+    E_MISSILE = 6,
+    E_LASER = 7,
+    E_INVALID = 8
   };
 
 #endif /* _E_ENTITYTYPE_H_ */
