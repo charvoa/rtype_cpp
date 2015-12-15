e0732160ed70f1fe44c09aafcfb385d779aee55c
Louis Audibert <louis.audibert@epitech.eu>
2015-12-15 05:29:47 -0800

    [*] Server : adding the right sprite for the Usain Bolt Bot

--- server/libs/bot_3.cpp
+++ server/libs/bot_3.cpp
@@ -5,7 +5,7 @@
 // Login   <audibel@epitech.net>
 //
 // Started on  Tue Dec 15 05:23:35 2015 Louis Audibert
-// Last update Tue Dec 15 05:25:51 2015 Louis Audibert
+// Last update Tue Dec 15 05:29:07 2015 Louis Audibert
 //
 
 #include <iostream>
@@ -14,7 +14,7 @@
 Bot::Bot(int id) : AEntity(id), _health(50), _x(275), _y(0)
 {
   std::cout << "Bot :: Bot3 :: Constructor" << std::endl;
-  _sprite = "sprite5.png";
+  _sprite = "sprite1.png";
   _name = _sprite;
   addSystem(C_HEALTH);
   addSystem(C_POSITION);
