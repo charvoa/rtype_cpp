7dc58de2ccb351d6d55929e62c37ed4e98fa5947
Nicolas charvoz <nicolas.charvoz@epitech.eu>
2015-12-16 17:53:30 +0100

    [*] Server : \\

--- server/libs/bot_3.cpp
+++ server/libs/bot_3.cpp
@@ -5,7 +5,7 @@
 // Login   <audibel@epitech.net>
 //
 // Started on  Tue Dec 15 05:23:35 2015 Louis Audibert
-// Last update Wed Dec 16 08:27:14 2015 Louis Audibert
+// Last update Wed Dec 16 17:36:57 2015 Nicolas Charvoz
 //
 
 #include <iostream>
@@ -36,8 +36,12 @@ void	Bot::generateY()
 
 void	Bot::update()
 {
-  _x--;
-  dynamic_cast<SystemPos*>(_systemManager.getSystemByComponent(C_POSITION))->update(_x, _y);
+  // _x--;
+  // dynamic_cast<SystemPos*>(_systemManager.getSystemByComponent(C_POSITION))->update(_x, _y);
+
+  ComponentPosition *pos = reinterpret_cast<ComponentPosition*>(this->getSystemManager()->getSystemByComponent(C_POSITION)->getComponent());
+  std::cout << "Position of bot3 >> " << this->getId() << ": " << pos->getX() << " ; " << pos->getY() << " << " << std::endl;
+
   // _x -= 5;
   // std::cout << "Bot :: Bot3 :: Update" << std::endl;
   // dynamic_cast<SystemPos*>(_systemManager.getSystemByComponent(C_POSITION))->update(_x, _y);
