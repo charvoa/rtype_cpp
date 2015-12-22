2b8652846ed670a61265a64a230d619fabdb2e7d
Antoine Garcia <antoine.garcia@epitech.eu>
2015-12-22 09:34:41 -0800

    Check executeCommand

--- server/Server.cpp
+++ server/Server.cpp
@@ -71,8 +71,8 @@ void Server::run()
 	continue;
       }
       ANetwork::t_frame *frame = reinterpret_cast<ANetwork::t_frame*>(data);
-      std::cout << frame->idRequest << std::endl;
-      this->_commandManager.executeCommand(*(reinterpret_cast<ANetwork::t_frame*>(data)),
+      if (frame->idRequest < 110)
+	this->_commandManager.executeCommand(*(reinterpret_cast<ANetwork::t_frame*>(data)),
 					   client, this);
     }
 }
