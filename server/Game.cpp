b3ed4601547992e14b6c85b61aec48e3ec4e7609
Nicolas charvoz <nicolas.charvoz@epitech.eu>
2015-12-27 18:21:15 +0100

    merge

--- server/Game.cpp
+++ server/Game.cpp
@@ -384,8 +384,8 @@ void Game::updateMonster()
     {
       ComponentPosition *pos = reinterpret_cast<ComponentPosition*>((*it)->getSystemManager()->getSystemByComponent(C_POSITION)->getComponent());
       reinterpret_cast<Bot*>(*it)->update();
-	  if (reinterpret_cast<Bot*>(*it)->isInScreen() == true)
-          shootBot(reinterpret_cast<Bot*>(*it));
+      if (reinterpret_cast<Bot*>(*it)->isInScreen() == true)
+	shootBot(reinterpret_cast<Bot*>(*it));
       //(*it)->update((*it)->refreshHitbox());
       if (pos->getX() < -100)
 	{
